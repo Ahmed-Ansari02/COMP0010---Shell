c1f0933228d2b6f554ffc38a70d6280580fcf2ac
Ahmed Ansari <ansari82002@gmail.com>
2023-11-29 13:01:24 +0000

    fixed cut command

--- src/Converter.py
+++ src/Converter.py
@@ -5,13 +5,14 @@ from Antlr.ShellGrammarVisitor import ShellGrammarVisitor
 from Applications import *
 from antlr4 import *
 
-class Converter(ShellGrammarVisitor):
 
+class Converter(ShellGrammarVisitor):
     def __init__(self):
         super().__init__()
-# Seq(Call(echo [hell, world]), Call(echo [hello, world]))
-# echo hell world ; echo hello world
-    def visitCommand(self, ctx:ShellGrammarParser.CommandContext):
+
+    # Seq(Call(echo [hell, world]), Call(echo [hello, world]))
+    # echo hell world ; echo hello world
+    def visitCommand(self, ctx: ShellGrammarParser.CommandContext):
         if ctx.call():
             return self.visit(ctx.call())
         elif ctx.pipe():
@@ -20,34 +21,37 @@ class Converter(ShellGrammarVisitor):
             return None
         else:
             return Seq(self.visit(ctx.command(0)), self.visit(ctx.command(1)))
-            
-    def visitPipe(self, ctx:ShellGrammarParser.PipeContext):
+
+    def visitPipe(self, ctx: ShellGrammarParser.PipeContext):
         if ctx.pipe():
             return Pipe(left=self.visit(ctx.pipe()), right=self.visit(ctx.call(0)))
         return Pipe(left=self.visit(ctx.call(0)), right=self.visit(ctx.call(1)))
-    
-    def visitCall(self, ctx:ShellGrammarParser.CallContext):
-        call_arr=[]
+
+    def visitCall(self, ctx: ShellGrammarParser.CallContext):
+        call_arr = []
         redirection_arr = []
         arguments = ctx.argument()
         for argument in arguments:
             if argument.quoted():
                 call_arr.append(self.visit(argument.quoted(0)))
-            elif "*" in argument.getText() or "?" in argument.getText() or "[" in argument.getText():
+            elif (
+                "*" in argument.getText()
+                or "?" in argument.getText()
+                or "[" in argument.getText()
+            ):
                 call_arr.append(Pattern(argument.getText()))
             else:
                 call_arr.append(argument.getText())
         if ctx.redirection():
             redirections = ctx.redirection()
-            if len(redirections)>1:
+            if len(redirections) > 1:
                 raise ValueError("Only one redirection may be used")
             for i in redirections:
-                
                 redirection_arr.append(i.getText()[0])
                 redirection_arr.append(i.argument().getText())
-            return Redirection(Call(call_arr),redirection_arr[0],redirection_arr[1])
+            return Redirection(Call(call_arr), redirection_arr[0], redirection_arr[1])
         return Call(call_arr)
-    
+
         # arguments = [argument.getText() if argument.getChildCount() == 0 else ''.join(argument) for argument in arguments]
         # print(arguments)
         # for argument in arguments:
@@ -58,15 +62,13 @@ class Converter(ShellGrammarVisitor):
         #     return self.visitQuoted(arguments[0].quoted(0))
         # return Call([argument.getText() if not argument.quoted() else self.visit(argument.quoted(0)) for argument in arguments])
 
-    def visitArgument(self, ctx:ShellGrammarParser.ArgumentContext):
+    def visitArgument(self, ctx: ShellGrammarParser.ArgumentContext):
         return self.visitChildren(ctx)
 
-    def visitQuoted(self, ctx:ShellGrammarParser.QuotedContext):
+    def visitQuoted(self, ctx: ShellGrammarParser.QuotedContext):
         if ctx.SINGLE_QUOTED():
             return SingleQuoted(ctx.getText())
         elif ctx.DOUBLE_QUOTED():
             return DoubleQuoted(ctx.getText())
         elif ctx.BACK_QUOTED():
             return BackQuoted(ctx.getText())
-
-
--- src/Evaluator.py
+++ src/Evaluator.py
@@ -52,7 +52,7 @@ class Evaluator(Visitor):
         elif arrow == "<":
             try:
                 with open(io_file,'r') as file:
-                    pass
+                    pass 
             except FileNotFoundError:
                 raise(f"file {io_file} not found")
             call_object.arguments.append(io_file)  
@@ -69,9 +69,7 @@ class Evaluator(Visitor):
         else:
             return f"{left.accept(self)} {right.accept(self)}"
 
-#echo `echo hello` worldwrodl `echo`
-# ['echo ', '`echo hello`', ' worldwrodl ', `echo`]
-#  echo hello
+
     def visit_single_quoted(self, quoted):
         return quoted.value[1:-1]
     
@@ -89,7 +87,6 @@ class Evaluator(Visitor):
         stdin = io.StringIO(left_result)
         pipe.right.arguments.append(stdin)
         return pipe.right.accept(self)          
-    
+        
     def visit_pattern(self, pattern):
-        # print(pattern.files)
         return ' '.join(pattern.files)
